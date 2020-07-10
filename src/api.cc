0471f62d5fad8aa3a3b9e222194d2156f82a8885
Antoine Pietri <antoine.pietri1@gmail.com>
2020-07-10 11:30:00 +0200

    api: dump game state unconditionally

--- src/api.cc
+++ src/api.cc
@@ -373,8 +373,5 @@ int Api::nombre_tours()
 //
 const char* Api::get_dump()
 {
-    if (player_->type == rules::SPECTATOR)
-        return dump_game_state(*game_state_, actions_);
-    else
-        return nullptr;
+    return dump_game_state(*game_state_, actions_);
 }
